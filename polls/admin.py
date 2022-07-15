f70384c65c26b8d471168f91ddc6362b3cdaf882
deq <prgupta@deqode.com>
2022-07-15 16:20:28 +0530

    update

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Choice,Question
 
-admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -14,6 +14,7 @@ from django.utils import timezone
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
+    # model=Question
 
     def get_queryset(self):
         """
@@ -28,13 +29,19 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
-    model = Question
+    # model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
