859919955b346d298bda00bb2ad9ebc06c81c269
deq <prgupta@deqode.com>
2022-07-18 17:34:59 +0530

    added models

--- polls/models.py
+++ polls/models.py
@@ -34,5 +34,50 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    class Meta:
+        ordering = ['votes']
+
     def __str__(self):
         return self.choice_text
+
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=128)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    people=models.Manager()
+
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def full_name(self):
+        return("%s : %s",self.name,self.shirt_size)
+
+    
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
+
+    class Meta:
+        abstract=True
+
+class AuthorManager(models.Manager):
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(role="Admin")
